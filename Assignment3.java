f82c66689bdb8bf3acfd01013fba8322e67ba114
gucearwel-alt <gucearwel@gmail.com>
2026-09-01 22:02:04 +0800

    Create Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+public class Main {
+public static void main(String[] args) {
+         String firstName = "Arwel";
+         String lastName = "Guce";
+         String address = "Villasis";
+         String mobileNumber = "09922747496";
+         int age = 18;
+         double gwa = 98.23;
+         System.out.println("Firstname : " + firstName);
+         System.out.println("Lastname : " + lastName);
+         System.out.println("Address : " + address);
+         System.out.println("Mobile Number : " + mobileNumber);
+         System.out.println("Age : " + age);
+         System.out.println("GWA : " + gwa);
+     }
+}
